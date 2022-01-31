be626dba525e1bdcdb8930ec5f9f719f0a671774
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2022-01-31 09:37:43 +0300

    refactor

--- src/Core/HepsiBurada.MarsRover.Application/Features/CQRS/MarsRoverCommand.cs
+++ src/Core/HepsiBurada.MarsRover.Application/Features/CQRS/MarsRoverCommand.cs
@@ -33,45 +33,45 @@ namespace HepsiBurada.MarsRover.Application.Features.CQRS
             {
                 foreach (var gesture in command.TravelerGestures)
                 {
-                    if (gesture == ActionValue.Left)
+                    switch (gesture)
                     {
-                        if (command.Traveler != null)
-                        {
-                            new DirectionStateContext(new LeftDirectionState()).Handle(command.Traveler);
-                        }
-                    }
-                    else if (gesture == ActionValue.Right)
-                    {
-                        if (command.Traveler != null)
-                        {
-                            new DirectionStateContext(new RightDirectionState()).Handle(command.Traveler);
+                        case ActionValue.Left:
+                            if (command.Traveler != null)
+                            {
+                                new DirectionStateContext(new LeftDirectionState()).Handle(command.Traveler);
+                            }
+                            break;
+                        case ActionValue.Right:
+                            if (command.Traveler != null)
+                            {
+                                new DirectionStateContext(new RightDirectionState()).Handle(command.Traveler);
+                            }
 
-                        }
-
-                    }
-
-                    else if (gesture == ActionValue.Move)
-                    {
-                        if (command.Traveler?.Direction == DirectionValue.East)
-                        {
-                            new MarsRoverEngineDirector(new EastMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
-
-                        }
-
-                        else if (command.Traveler?.Direction == DirectionValue.North)
-                        {
-                            new MarsRoverEngineDirector(new NorthMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
-                        }
-                        else if (command.Traveler?.Direction == DirectionValue.South)
-                        {
-                            new MarsRoverEngineDirector(new SouthMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
-                        }
-                        else if (command.Traveler?.Direction == DirectionValue.West)
-                        {
-                            new MarsRoverEngineDirector(new WestMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
-                        }
+                            break;
+                        case ActionValue.Move:
+                            CallAbstractFactory(command);
+                            break;
                     }
                 }
+            }
+        }
+
+        private static void CallAbstractFactory(MarsRoverCommand command)
+        {
+            switch (command.Traveler?.Direction)
+            {
+                case DirectionValue.East:
+                    new MarsRoverEngineDirector(new EastMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
+                    break;
+                case DirectionValue.North:
+                    new MarsRoverEngineDirector(new NorthMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
+                    break;
+                case DirectionValue.South:
+                    new MarsRoverEngineDirector(new SouthMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
+                    break;
+                case DirectionValue.West:
+                    new MarsRoverEngineDirector(new WestMarsRoverEngineFactory(), command.PlateX, command.PlateY).Process(command.Traveler);
+                    break;
             }
         }
     }
